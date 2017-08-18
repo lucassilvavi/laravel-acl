67b4fae2eb125e4f39f9fe42b0301209befb4a0a
Lucas Vieira <lucas.vieira789@hotmail.com>
2017-08-17 21:30:18 -0300

    aula numero 7

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-@forelse($dados['posts'] as $post)
-    <h1>{{$post->title}}</h1>
-    <p>{{$post->description}}</p><hr>
-    <p>Autor:{{$post->user->name}}</p>
-    <a href="{{url("post/$post->id/update")}}">Editar</a>
-    @empty
-    <p>Nenhum Post Cadastrado</p>
-    @endforelse
-</div>
+    <div class="container">
+        @forelse($dados['posts'] as $post)
+
+            <h1>{{$post->title}}</h1>
+            <p>{{$post->description}}</p>
+            <hr>
+            <p>Autor:{{$post->user->name}}</p>
+            @can('update-post',$post)
+                <a href="{{url("post/$post->id/update")}}">Editar</a>
+            @endcan
+        @empty
+            <p>Nenhum Post Cadastrado</p>
+        @endforelse
+    </div>
 @endsection
